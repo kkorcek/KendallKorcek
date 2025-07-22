695e442062db220f4f69f736102fd01e61cb0f7a
kendallkorcek <korcekke@msu.edu>
2025-07-22 11:39:39 -0400

    Test

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
-// import { useEffect, useState } from "react";
-// import type { Schema } from "../amplify/data/resource";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import { generateClient } from "aws-amplify/data";
 import './App.css';
 import Navbar from './components/navbar';
 import Home from './pages/Home';
